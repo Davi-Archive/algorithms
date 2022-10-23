21d7108842bce9ab1abb918d8eff42e92bbe79f0
davi38 <davi4alves@gmail.com>
2022-10-23 16:25:21 -0300

    feat: new solution

---
+++ arrSum.js
@@ -0,0 +1,9 @@
+function simpleArraySum(ar) { // Write your code here
+   let value = 0;
+    for (const item of ar) {
+    value += item
+    }
+    console.log(value)
+}
+
+simpleArraySum([1,2,3,4])
