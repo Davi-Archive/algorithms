4a258ca96efc0e55f9f873182928e38dd6dc30af
davi38 <davi4alves@gmail.com>
2022-10-23 16:56:56 -0300

    feat: better algorithm

--- multiples.js
+++ multiples.js
@@ -1,21 +1,11 @@
 function solution(number) {
   let soma = 0;
   for (let i = 1; i < number; i++) {
-    if (number < 0) {
-      soma = 0;
-    } else {
-      if (i % 3 == 0 && i % 5 == 0) {
-        soma += i;
-      } else {
-        if (i % 3 == 0) {
-          soma += i;
-        }
-        if (i % 5 == 0) {
-          soma += i;
-        }
-      }
+    if (i % 3 == 0 || i % 5 == 0) {
+      soma += i;
     }
   }
+  console.log(soma);
   return soma;
 }
 
