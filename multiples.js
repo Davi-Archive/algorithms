ffc41773e1e7f6e618bb6797101f42bd31c43ef7
davi38 <davi4alves@gmail.com>
2022-10-23 16:53:43 -0300

    feat: new algorithm

--- multiples.js
+++ multiples.js
@@ -1,5 +1,22 @@
 function solution(number) {
-  console.log(number%5)
+  let soma = 0;
+  for (let i = 1; i < number; i++) {
+    if (number < 0) {
+      soma = 0;
+    } else {
+      if (i % 3 == 0 && i % 5 == 0) {
+        soma += i;
+      } else {
+        if (i % 3 == 0) {
+          soma += i;
+        }
+        if (i % 5 == 0) {
+          soma += i;
+        }
+      }
+    }
+  }
+  return soma;
 }
 
 solution(10);
